1ed3ad2f207bdaea4d85197d93204c8f4063a7df
ohbabsa-debuger <ohbabsa2025@gmail.com>
2025-09-10 10:23:23 +0900

    20250910.10.22_Init
    깃허브 연동 테스트

---
+++ Assets/GitHubTest.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GitHubTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //열어주세요
+        //닫아주세요
+        //나가주세요
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
